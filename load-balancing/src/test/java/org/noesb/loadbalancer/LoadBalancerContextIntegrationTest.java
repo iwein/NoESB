cda5be7635dd2bf5639e4007091f31969602d024
iwein <iwein@fuld.net>
2010-03-15 14:54:01 +0100

    added context test for load balancer

---
+++ load-balancing/src/test/java/org/noesb/loadbalancer/LoadBalancerContextIntegrationTest.java
@@ -0,0 +1,19 @@
+package org.noesb.loadbalancer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring/module-context.xml"})
+public class LoadBalancerContextIntegrationTest {
+
+    @Test
+    public void shouldLoadConfig() {
+        //spring loads the configuration
+    }
+}
